7cff8d329fcbb6c6b9e26e2b2b2081096a55a1df
0xJerry <jeromendrw@gmail.com>
2025-11-01 02:07:54 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -135,19 +135,14 @@ export default function RootLayout({
           strategy="lazyOnload"
         />
         <Script
-          id="kofi-widget"
-          strategy="lazyOnload"
-          dangerouslySetInnerHTML={{
-            __html: `
-              kofiWidgetOverlay.draw('andres__', {
-                'type': 'floating-chat',
-                'floating-chat.donateButton.text': 'Support me',
-                'floating-chat.donateButton.background-color': '#5cb85c',
-                'floating-chat.donateButton.text-color': '#fff'
-              });
-            `
-          }}
-        />
+  src="https://storage.ko-fi.com/cdn/scripts/overlay-widget.js"
+  strategy="afterInteractive"
+/>
+<Script
+  id="kofi-widget"
+  strategy="afterInteractive"
+  dangerouslySetInnerHTML={{...}}
+/>
         
         <RootLayoutContent>{children}</RootLayoutContent>
       </body>
